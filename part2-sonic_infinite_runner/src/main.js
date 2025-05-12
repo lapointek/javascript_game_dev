da2e336747b5b245107f332f0e86199d3a33f5d9
lapointek <156107790+lapointek@users.noreply.github.com>
2025-05-12 18:12:20 -0400

    added loading sprite in main.js

--- part2-sonic_infinite_runner/src/main.js
+++ part2-sonic_infinite_runner/src/main.js
@@ -1,1 +1,28 @@
 import k from "./kaplayCtx";
+
+// import asset
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
+k.loadSprite("platforms", "graphics/platforms-bg.png");
+k.loadSprite("sonic", "graphics/sonic.png", {
+  // how many frames per row
+  sliceX: 8,
+  // how many frames per column
+  sliceY: 2,
+  // config animations starting at zero
+  anims: {
+    // first row
+    run: { from: 0, to: 7, loop: true, speed: 30 },
+    jump: { from: 8, to: 15, loop: true, speed: 100 },
+  },
+});
+k.loadSprite("motobug", "graphics/motobug.png", {
+  // how many frames per row
+  sliceX: 8,
+  // how many frames per column
+  sliceY: 2,
+  // config animations starting at zero
+  anims: {
+    // first row
+    run: { from: 0, to: 4, loop: true, speed: 8 },
+  },
+});
