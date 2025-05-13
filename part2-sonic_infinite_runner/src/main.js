eb7502db4c7c61413488298a94de7383eea59862
lapointek <156107790+lapointek@users.noreply.github.com>
2025-05-13 13:43:53 -0400

    added mainMenu.js and loadSound, scene and loadFont in main.js

--- part2-sonic_infinite_runner/src/main.js
+++ part2-sonic_infinite_runner/src/main.js
@@ -1,39 +1,56 @@
 import k from "./kaplayCtx";
+import mainMenu from "./scenes/mainMenu";
 
 // import asset
 k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
-k.loadSprite("platforms", "graphics/platforms-bg.png");
+k.loadSprite("platforms", "graphics/platforms.png");
 k.loadSprite("sonic", "graphics/sonic.png", {
-  // how many frames per row
-  sliceX: 8,
-  // how many frames per column
-  sliceY: 2,
-  // config animations starting at zero
-  anims: {
-    // first row
-    run: { from: 0, to: 7, loop: true, speed: 30 },
-    jump: { from: 8, to: 15, loop: true, speed: 100 },
-  },
+    // how many frames per row
+    sliceX: 8,
+    // how many frames per column
+    sliceY: 2,
+    // config animations starting at zero
+    anims: {
+        // first row
+        run: { from: 0, to: 7, loop: true, speed: 30 },
+        jump: { from: 8, to: 15, loop: true, speed: 100 },
+    },
 });
 k.loadSprite("ring", "graphics/ring.png", {
-  // how many frames per row
-  sliceX: 16,
-  // how many frames per column
-  sliceY: 1,
-  // config animations starting at zero
-  anims: {
-    // first row
-    run: { from: 0, to: 15, loop: true, speed: 30 },
-  },
+    // how many frames per row
+    sliceX: 16,
+    // how many frames per column
+    sliceY: 1,
+    // config animations starting at zero
+    anims: {
+        // first row
+        run: { from: 0, to: 15, loop: true, speed: 30 },
+    },
 });
 k.loadSprite("motobug", "graphics/motobug.png", {
-  // how many frames per row
-  sliceX: 5,
-  // how many frames per column
-  sliceY: 1,
-  // config animations starting at zero
-  anims: {
-    // first row
-    run: { from: 0, to: 4, loop: true, speed: 8 },
-  },
+    // how many frames per row
+    sliceX: 5,
+    // how many frames per column
+    sliceY: 1,
+    // config animations starting at zero
+    anims: {
+        // first row
+        run: { from: 0, to: 4, loop: true, speed: 8 },
+    },
 });
+k.loadFont("mania", "fonts/mania.ttf");
+
+k.loadSound("destroy", "sounds/Destroy.wav");
+k.loadSound("hurt", "sounds/Hurt.wav");
+k.loadSound("hyper-ring", "sounds/HyperRing.wav");
+k.loadSound("jump", "sounds/Jump.wav");
+k.loadSound("ring", "sounds/Ring.wav");
+k.loadSound("city", "sounds/city.wav");
+
+k.scene("main-menu", mainMenu);
+
+k.scene("game", () => {});
+k.scene("gameover", () => {});
+
+// default scene
+k.go("main-menu");
---
+++ part2-sonic_infinite_runner/src/scenes/mainMenu.js
@@ -0,0 +1,41 @@
+import k from "../kaplayCtx";
+
+export default function mainMenu() {
+    // if best-score doesnt exist than create best-score set to zero
+    if (!k.getData("best-score")) k.setData("best-score", 0);
+    k.onButtonPress("jump", () => k.go("game"));
+
+    // background piece
+    const bgPieceWidth = 1920;
+    const bgPieces = [
+        // add component to scene and determine position
+        k.add([
+            k.sprite("chemical-bg"),
+            k.pos(0, 0),
+            k.scale(2),
+            k.opacity(0.8),
+        ]),
+        k.add([
+            k.sprite("chemical-bg"),
+            k.pos(bgPieceWidth * 2, 0),
+            k.scale(2),
+            k.opacity(0.8),
+        ]),
+    ];
+
+    const platformWidth = 1280;
+    const platforms = [
+        k.add([k.sprite("platforms"), k.pos(0, 450), k.scale(4)]),
+        k.add([
+            k.sprite("platforms"),
+            k.pos(platformWidth * 4, 450),
+            k.scale(4),
+        ]),
+
+        k.onUpdate(() => {
+            if (bgPieces[1].pos.x < 0) {
+                bg;
+            }
+        }),
+    ];
+}
