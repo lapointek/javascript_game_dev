ba6b158e82d671591dcdaebc8bbdb46305abc115
lapointek <156107790+lapointek@users.noreply.github.com>
2025-05-15 12:55:16 -0400

    addd styling to make background black in index.html, added scaling to canvas in kaboomLoader.js

--- part3-metroidvania_style_game/src/kaboomLoader.js
+++ part3-metroidvania_style_game/src/kaboomLoader.js
@@ -3,8 +3,9 @@ import kaboom from "../lib/kaboom.mjs";
 export const scale = 2;
 // game canvas
 export const k = kaboom({
-    width: 640,
-    height: 360,
+    width: 640 * scale,
+    height: 360 * scale,
+    scale,
     letterbox: true,
     global: false,
 });
