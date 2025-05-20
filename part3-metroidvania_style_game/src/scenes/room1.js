0186f17c366401ab8af8502bfd1edfa08950094a
lapointek <156107790+lapointek@users.noreply.github.com>
2025-05-20 14:36:42 -0400

    added setMapColliders() function to roomUtils.js and room1.js

--- part3-metroidvania_style_game/src/scenes/room1.js
+++ part3-metroidvania_style_game/src/scenes/room1.js
@@ -1,8 +1,19 @@
-import { setBackgroundColor } from "./roomUtils.js";
+import { setBackgroundColor, setMapColliders } from "./roomUtils.js";
 
 export function room1(k, roomData) {
     // color background of canvas
     setBackgroundColor(k, "#a2aed5");
 
-    const roomLayers = roomData.roomLayers;
+    const roomLayers = roomData.layers;
+
+    const map = k.add([k.pos(0, 0), k.sprite("room1")]);
+    const colliders = [];
+    for (const layer of roomLayers) {
+        if (layer.name === "colliders") {
+            // pushing all roomdData layers into colliders array
+            colliders.push(...layer.objects);
+            break;
+        }
+    }
+    setMapColliders(k, map, colliders);
 }
--- part3-metroidvania_style_game/src/scenes/roomUtils.js
+++ part3-metroidvania_style_game/src/scenes/roomUtils.js
@@ -7,3 +7,44 @@ export function setBackgroundColor(k, hexColorCode) {
         k.fixed(),
     ]);
 }
+
+export function setMapColliders(k, map, colliders) {
+    for (const collider of colliders) {
+        if (collider.polygon) {
+            const coordinates = [];
+            for (const point of collider.polygon) {
+                coordinates.push(k.vec2(point.x, point.y));
+            }
+
+            map.add([
+                k.pos(collider.x, collider.y),
+                // hit box
+                k.area({
+                    // takes a vector2
+                    shape: new k.Polygon(coordinates),
+                    // tag to ignore collider when colliding with eachother
+                    collisionIgnore: ["collider"],
+                }),
+                // tag name
+                "collider",
+                collider.type,
+            ]);
+            continue;
+        }
+        if (collider.name === "boss-barrier") {
+            // TODO
+            continue;
+        }
+        map.add([
+            k.pos(collider.x, collider.y),
+            k.area({
+                shape: new k.rect(k.vec2(0), collider.width, collider.height),
+                collisionIgnore: ["collider"],
+            }),
+            // body act as a wall
+            k.body({ isStatic: true }),
+            "collider",
+            collider.type,
+        ]);
+    }
+}
